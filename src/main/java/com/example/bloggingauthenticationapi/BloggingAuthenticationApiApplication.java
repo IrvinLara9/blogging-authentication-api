8b97e8e6ec29984d9fed9ca7df21d3741dcb7c9f
Irvin Lara <irvinlaravillalobos@yahoo.com.mx>
2023-08-09 00:39:23 -0600

    Modify compose.yaml

---
+++ src/main/java/com/example/bloggingauthenticationapi/BloggingAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloggingauthenticationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingAuthenticationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloggingauthenticationapi/BloggingAuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloggingauthenticationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAuthenticationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
